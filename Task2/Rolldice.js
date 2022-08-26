00b45d7ee6e348e4f6605da248968449d3f97889
Membah <87417620+Membah@users.noreply.github.com>
2022-08-26 20:54:40 +0200

    Update Rolldice.js

--- Task2/Rolldice.js
+++ Task2/Rolldice.js
@@ -1,1 +1,15 @@
+document.getElementById('rollButton').addEventListener('Click', startGame);
+function startGame(){
+    var WinningCategories = [
+        "Best Hackhathon solution",
+        "Best Enterprise Solution",
+        "Best Educational Solution",
+        "Best Gaming Solution",
+        "Best Health Solution",       
+        "Best Campus Cup Solution",
+    ]
+    let Categories = WinningCategories[Math.floor(Math.random() * WinningCategories.length)]
 
+    console.log(Categories)
+
+}
